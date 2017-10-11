34865437b97e4f15014f008496fe9a2f7c49137f
David Tolnay <dtolnay@gmail.com>
2017-10-11 09:55:37 -0700

    Copy documentation to rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,49 @@
+//! This crate provides the following three functions:
+//!
+//! ```rust
+//! # trait Ignore {
+//! fn stdin_isatty() -> bool
+//! # ;
+//! fn stdout_isatty() -> bool
+//! # ;
+//! fn stderr_isatty() -> bool
+//! # ;
+//! # }
+//! ```
+//!
+//! On Linux and Mac they are implemented with [`libc::isatty`]. On Windows they
+//! are implemented with [`kernel32::GetConsoleMode`].
+//!
+//! [`libc::isatty`]: http://man7.org/linux/man-pages/man3/isatty.3.html
+//! [`kernel32::GetConsoleMode`]: https://msdn.microsoft.com/en-us/library/windows/desktop/ms683167.aspx
+//!
+//! The `stdin_isatty` function is not yet implemented for Windows. If you need
+//! it, please check [dtolnay/isatty#1] and contribute an implementation!
+//!
+//! [dtolnay/isatty#1]: https://github.com/dtolnay/isatty/issues/1
+//!
+//! ## Usage
+//!
+//! `Cargo.toml`
+//!
+//! > ```toml
+//! > [dependencies]
+//! > isatty = "0.1"
+//! > ```
+//!
+//! `src/main.rs`
+//!
+//! > ```rust
+//! > extern crate isatty;
+//! > use isatty::{stdin_isatty, stdout_isatty, stderr_isatty};
+//! >
+//! > fn main() {
+//! >     println!("stdin: {}", stdin_isatty());
+//! >     println!("stdout: {}", stdout_isatty());
+//! >     println!("stderr: {}", stderr_isatty());
+//! > }
+//! > ```
+
 // Based on:
 //  - https://github.com/rust-lang/cargo/blob/099ad28104fe319f493dc42e0c694d468c65767d/src/cargo/lib.rs#L154-L178
 //  - https://github.com/BurntSushi/ripgrep/issues/94#issuecomment-261761687
